f4ca6aa415fdcf048270a5d526fef9c8edc2a6b4
Akash Kumar Chaurasiya <akashchaurasiya5904@gmail.com>
2024-12-30 16:23:54 +0530

    finishing the project "MY Shopping list APP"

--- app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
@@ -5,24 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.myshoppinglistapp.ui.theme.MyShoppingLIstAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,29 +14,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyShoppingLIstAppTheme {
-                    var sItem by remember { mutableStateOf(listOf<ShoppingItem>()) }
-                    Column(modifier = Modifier.fillMaxSize(),
-                        verticalArrangement =  Arrangement.Center) {
-                        Button(onClick = {},
-                            modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                            Text( "Add Item!")
-                        }
-
-                        LazyColumn(
-                            modifier = Modifier.fillMaxSize().padding(24.dp)
-                        ) {
-
-                        }
-                    }
+                ShoppingListApp()
                 }
             }
         }
     }
 
-data class ShoppingItem(
-    val id: Int,
-    var name: String,
-    var quantity: Int,
-    val isEditing: Boolean = false,
-)
+
 
